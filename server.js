05cae5d7fa0fb3821214dd70f4f33875e44763b6
Jhon Francia Minaya <jfranciaminaya@gmail.com>
2025-10-06 09:46:49 -0500

    Frontend terminado

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express')
+
+//Actualización para desplegar el FRONT-END
+const cors = require('cors') //Permisos sobre el contenido a desplegar
+const path = require('path') //Express servir el frontend
+
 const productoRoutes = require('./routes/productoRoutes')
 
 const app = express()
 const PORT = process.env.PORT || 3000 //Puerto de la App
+
+//Actualización - Permisos cors
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+}))
+
+//Actualización:
+//Servir los documentos HTML, CSS, JS
+app.use(express.static(path.join(__dirname, 'public')))
+
+//http://localhost:3000 -> public>index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 //Comunicación se realizará JSON
 app.use(express.json())
